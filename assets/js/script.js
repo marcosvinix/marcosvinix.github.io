78a6f6671b72c5e325c99f3529e914672ba88a98
Marcos Vinicius <marcos1vinicius2@hotmail.com>
2023-12-04 12:57:03 -0300

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,23 @@ let btn_ex = document.querySelector('.btn-ex');
 let btn_ts = document.querySelector('.btn-ts');
 let title = document.querySelector('.title');
 
-btn_ex.addEventListener('click', () => {
+const exercises = () => {
     ts.style.display = 'none'
     ex.style.display = 'block'
     title.innerHTML = 'Exercícios'
-})
+    document.cookie = "selected = exercises";
+}
 
-btn_ts.addEventListener('click', () => {
+const tests = () => {
     ex.style.display = 'none'
     ts.style.display = 'block'
     title.innerHTML = 'Testes'
-})
+    document.cookie = "selected = tests";
+}
+
+btn_ex.addEventListener('click',exercises)
+btn_ts.addEventListener('click',tests)
+
+if(document.cookie.includes('tests')){
+    tests()
+}
